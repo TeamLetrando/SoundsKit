71da62b9d62e7e97df7a238547cb546e0dd35eef
Patricia <patysiq@yahoo.com.br>
2021-10-09 13:37:18 -0300

    feature: add some comments

--- Sources/SoundsKit/SoundsKit.swift
+++ Sources/SoundsKit/SoundsKit.swift
@@ -15,20 +15,23 @@ open class SoundsKit {
         self.fileExtension = fileExtention
     }
     
-    ///
+    
+    /// Set first time of sound
+    /// - Parameter key: Choose the first state of sound and add  this function into you launch funcion at AppDelegate
     public func setKeyAudio(_ key: Bool){
         userDefaults.set(key, forKey: "sound")
     }
 
     /// Globally check enable or disable sound.
+    /// - Returns: Return `True` if sound is on or `False` if sound is off.
     public func audioIsOn() -> Bool {
         if userDefaults.object(forKey: "sound") == nil {
             setKeyAudio(true)
         }
         return userDefaults.bool(forKey: "sound")
     }
-    
     /// Play sound from a sound file.
+    /// - Parameter bundle: It's for default a main bundle, but if you need to customize, it's possible with this parameter.
     public func play(bundle: Bundle = Bundle.main) throws {
             /// Sound session. The default value is the shared `AVAudioSession` session with `ambient` category.
             let audioSession = AVAudioSession.sharedInstance()
@@ -53,15 +56,17 @@ open class SoundsKit {
         setKeyAudio(false)
         audioPlayer?.stop()
     }
-    
+
     /// Reproduce speech from a word or letter.
+    /// - Parameters:
+    ///   - text: the text if you want to speech
+    ///   - language: It's for default a  `Portuguese language`, but if you need to customize, it's possible with this parameter.
     @discardableResult
-    public func reproduceSpeech(_ text:String, language: String = "pt-BR", delegate: AVSpeechSynthesizerDelegate? = nil) -> AVSpeechSynthesizer {
+    public func reproduceSpeech(_ text:String, language: String = "pt-BR") -> AVSpeechSynthesizer {
         let utterance =  AVSpeechUtterance(string: text)
         let voice = AVSpeechSynthesisVoice(language: language)
         utterance.voice = voice
         let synthesizer = AVSpeechSynthesizer()
-        synthesizer.delegate = delegate
         synthesizer.speak(utterance)
         return synthesizer
     }
